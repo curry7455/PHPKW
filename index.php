4ba8338259cba9c69ea561072405c2e5a85fca35
curry7455 <156011654+curry7455@users.noreply.github.com>
2024-10-10 22:54:23 -0400

    indexed

--- index.php
+++ index.php
@@ -3,31 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-        </style>
+    <title>Kerri Wilkinson - My PHP Projects</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header>
-        <h1>
-            <?= "Hello, World!" ?>
-        </h1>
-    </header>
-    <p>
+    <div class="container">
+        
+        <header>
+            <h1>Kerri Wilkinson</h1>
+            <hr>
+            <h2>My PHP Projects</h2>
+        </header>
+
+        <!-- projects section -->
+        <section>
+            <ul>
+                <!-- add links for projects here -->
+                <li><a href="week1/">Week 1</a></li>
+            </ul>
+        </section>
+    </div>
 </body>
 </html>
-
-
-
-<ul>
-            <?php foreach ($task as $detail => $val) : ?>
-                <li>
-                    <strong><?= ucwords($detail); ?></strong> <?= $val; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
